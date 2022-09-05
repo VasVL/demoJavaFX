4ba3304332cf47f2800d2f98e7b2ab906b0f2f79
VasVL <70405415+VasVL@users.noreply.github.com>
2022-09-05 20:40:57 +0300

    Switch 2 Scenes

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
