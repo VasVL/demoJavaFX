89d44c8dfaeb1794d96ef94d3fb391c41053c8a9
VasVL <70405415+VasVL@users.noreply.github.com>
2022-09-05 14:54:12 +0300

    text line shapes image

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -27,7 +32,51 @@ public class Main extends Application {
         text.setFont(Font.font("Verdana", 24));
         text.setFill(Color.LIGHTGRAY);
 
+        Line line = new Line();
+        line.setStartX(200);
+        line.setStartY(200);
+        line.setEndX(500);
+        line.setEndY(200);
+        line.setStrokeWidth(5);
+        line.setStroke(Color.RED);
+        line.setOpacity(0.5);
+        line.setRotate(45);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setX(100);
+        rectangle.setY(100);
+        rectangle.setWidth(100);
+        rectangle.setHeight(100);
+        rectangle.setFill(Color.LIGHTBLUE);
+        rectangle.setStroke(Color.BLACK);
+        rectangle.setStrokeWidth(5);
+
+        Polygon triangle = new Polygon();
+        triangle.getPoints().setAll(
+                200.0, 200.0,
+                300.0, 300.0,
+                200.0, 300.0
+        );
+        triangle.setFill(Color.YELLOW);
+
+        Circle circle = new Circle();
+        circle.setCenterX(350);
+        circle.setCenterY(350);
+        circle.setRadius(50);
+        circle.setFill(Color.ORANGE);
+
+        Image image = new Image("img.png");
+        ImageView imageView = new ImageView(image);
+        imageView.setX(600);
+        imageView.setY(200);
+
         root.getChildren().add(text);
+        root.getChildren().add(line);
+        root.getChildren().add(rectangle);
+        root.getChildren().add(triangle);
+        root.getChildren().add(circle);
+        root.getChildren().add(imageView);
+
         stage.setScene(scene);
         stage.show();
     }
