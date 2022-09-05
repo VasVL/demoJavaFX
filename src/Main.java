6a9663d446964540f325600f28b77a1040434644
VasVL <70405415+VasVL@users.noreply.github.com>
2022-09-05 14:22:36 +0300

    text

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -16,13 +17,15 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Group root = new Group();
-        Scene scene = new Scene(root, 600, 600, Color.DARKGRAY);
+        Scene scene = new Scene(root, 1000, 600, Color.DIMGRAY);
         Stage stage = new Stage();
 
         Text text = new Text();
         text.setText("Жить в эпоху свершений, имея возвышенный нрав, к сожалению трудно");
         text.setX(50);
         text.setY(50);
+        text.setFont(Font.font("Verdana", 24));
+        text.setFill(Color.LIGHTGRAY);
 
         root.getChildren().add(text);
         stage.setScene(scene);
