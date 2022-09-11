ef169176365378f54311b0998c1ddc6582ae8513
VasVL <70405415+VasVL@users.noreply.github.com>
2022-09-11 17:24:28 +0300

    Logout Работает для кнопки, крестика и alt+F4

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
@@ -22,5 +25,23 @@ public class Main extends Application {
 
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        primaryStage.setOnCloseRequest(event -> {
+            event.consume();
+            logout(primaryStage);
+        });
+    }
+
+    public void logout(Stage stage){
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You're about to logout!");
+        alert.setContentText("Do you want to save before exiting?");
+
+        if(alert.showAndWait().get() == ButtonType.OK) {
+            System.out.println("You've successfully logged out!");
+            stage.close();
+        }
     }
 }
