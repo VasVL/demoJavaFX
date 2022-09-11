d523b5f08d8c880d4476f3147df3ccb5061ebe38
VasVL <70405415+VasVL@users.noreply.github.com>
2022-09-11 16:55:58 +0300

    Switch 2 Controllers В качестве дз можно сделать кнопку 'reset'

--- src/SceneController.java
+++ src/SceneController.java
@@ -1,28 +1,35 @@
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 
 public class SceneController {
+
+    @FXML
+    TextField nameTextField;
+
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    public void switchToScene1(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
+    public void login(ActionEvent event) throws IOException {
 
-    public void switchToScene2(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
+        String username = nameTextField.getText();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene2.fxml"));
+        root = loader.load();
+
+        Scene2Controller scene2Controller = loader.getController();
+        scene2Controller.displayName(username);
+
+        //root = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
